acaaa87c332b8ef5110c188c3281e24d218a286b
Fritz Lin <uxfritz@163.com>
2017-03-19 16:34:44 +0800

    fix: ava test should be run serially

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ let test = require('ava')
 let fs = require('fs-extra-promise')
 let { exec } = require('child_process')
 let { join, dirname } = require('path')
-let  _7z = require('./')['7z']
 
-test('should get path of 7z.exe', async t => {
+test.serial('should get path of 7z.exe', async t => {
+  let _7z = require('./')['7z']
   let _7z_exe = join(__dirname, '7zip-lite', '7z.exe')
   t.is(_7z, _7z_exe)
 
@@ -12,10 +12,16 @@ test('should get path of 7z.exe', async t => {
   t.true(exists)
 })
 
-// dependent cross-unzip should work
-// inside which win-7zip should link to current project
-// ie. ln -s $(pwd) node_modules/cross-unzip/node_modules/win-7zip
-let src = __dirname
-let dest = join(__dirname, 'node_modules', 'cross-unzip', 'node_modules', 'win-7zip')
-fs.ensureSymlinkSync(src, dest, 'junction')
-require('cross-unzip/test/test')
+if (process.platform !== 'win32') {
+  console.warn('further test should be run in windows')
+} else {
+
+  // dependent cross-unzip should work
+  // inside which win-7zip should link to current project
+  // ie. ln -s $(pwd) node_modules/cross-unzip/node_modules/win-7zip
+  let src = __dirname
+  let dest = join(__dirname, 'node_modules', 'cross-unzip', 'node_modules', 'win-7zip')
+
+  fs.ensureSymlinkSync(src, dest, 'junction')
+  require('cross-unzip/test/test')
+}
